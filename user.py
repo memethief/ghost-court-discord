a218d3df1112da5f024492047d2cf826be7a6ec9
Dave Leaman <github@memethief.com>
2020-05-02 21:12:58 -0400

    Functioning enqueue command

--- user.py
+++ user.py
@@ -41,3 +41,48 @@ class UserCog(commands.Cog, name="Commands"):
         for msg in msg_response:
             debug("sending message...")
             await ctx.send(msg)
+
+    @commands.command(name='q')
+    async def enqueue(ctx, *roles):
+        '''
+        Add yourself to a queue or queues
+        
+        By itself this command will add you to all queues. You
+        can limit this to specific queues by following the
+        command with one or more role names. For example, if
+        you want to be added only to the plaintiff and clerk
+        queues, you may type:
+
+            !q plaintiff clerk
+
+        You may also use aggregate role names here.
+
+        Note that this command does not affect your current
+        place in any queues you have already joined. If you
+        want to move to the back of a queue you must use !dq,
+        followed by !q.
+        '''
+        debug('Got request to enqueue for {0}', roles)
+        user = ctx.author
+
+        if len(roles) == 0:
+            debug("Let's enqueue everywhere!")
+            roles = ['all']
+
+        for role in translate_roles(roles):
+            debug("Processing role {0}", role)
+            q = qs.get(role)
+
+            if q == None:
+                debug('Bad role {0} requested', role)
+                continue
+
+            if user in q:
+                debug('User {0} already in {1} queue', user, role)
+                continue
+
+            debug("Appending user to {0} queue", role)
+            q.append(user)
+
+        debug('Finished adding to queues')
+        await ctx.send(list_user_queue(user))
