d467c55ce2eaa50af614bbbcb51311b114cca491
Dave Leaman <github@memethief.com>
2020-05-24 12:34:49 -0400

    Load cases from file
    Create four buckets for cases in the CaseQueue class: hopper, docket, current and history
    Read YAML file containing case descriptions
    Populate hopper with cases
    "status" method to output the current contents of each bucket

--- casequeue.py
+++ casequeue.py
@@ -1,26 +1,58 @@
 # Class representing the case queue
 from ghostcourt import debug, debug_obj
+from case import Case
+import yaml
 
 class CaseQueue(object):
     '''
     Singleton class representing the queue of cases
     '''
     __instance = None
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if CaseQueue.__instance is None:
             CaseQueue.__instance = object.__new__(cls)
-            CaseQueue.__instance.q = []
         return CaseQueue.__instance
 
-    cases = []
-    current_case = {
-        'docketNumber': None,
-        'plaintiffName': None,
-        'plaintiffUser': None,
-        'defendantName': None,
-        'defendantUser': None,
-        'judgeUser': None,
-    }
+    # case definitions available
+    hopper = []
+    # queue of upcoming cases
+    docket = []
+    # currently active case
+    current = None
+    # history of cases seen
+    history = []
+
+    def load(self, filename):
+        '''
+        Load case definitions from file
+        '''
+        try:
+            stream = open(filename, 'r')
+        except Exception as e:
+            print("Error encountered parsing case file: {0}".format(filename))
+            raise e
+
+        with stream as casefile:
+            for entry in yaml.safe_load_all(casefile):
+                case = Case(entry)
+                if case:
+                    CaseQueue.hopper.append(case)
+
+    def status(self):
+        yield "Cases in the hopper: {0}".format(len(CaseQueue.hopper))
+        for case in CaseQueue.hopper:
+            yield "> {0}: {1}".format(case.case, case.title)
+        yield "Cases in the docket: {0}".format(len(CaseQueue.docket))
+        for case in CaseQueue.docket:
+            yield "> {0}: {1}".format(case.case, case.title)
+        yield "Current case:"
+        if CaseQueue.current:
+            yield "> {0}: {1}".format(CaseQueue.current.case, CaseQueue.current.title)
+        else:
+            yield "None"
+        yield "Cases completed this session: {0}".format(len(CaseQueue.history))
+        for case in CaseQueue.history:
+            yield "> {0}: {1}".format(case.case, case.title)
 
     def add(self, case):
         '''
