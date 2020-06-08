8fcef1989cedc99c77f2ab076b514f3f41e0f008
Dave Leaman <github@memethief.com>
2020-06-08 01:56:16 -0400

    Add error handling support to qcase command

--- clerk.py
+++ clerk.py
@@ -7,20 +7,25 @@ from rolequeue import RoleQueue
 class ClerkCog(commands.Cog, name="Clerk commands"):
     def __init__(self, bot):
         self.bot = bot
+        self.caseq = CaseQueue()
         self.roleq = RoleQueue()
         debug("Clerk cog started")
 
     @commands.has_role('Clerk')
     @commands.command(name='qcase')
     async def enqueue_case(self, ctx):
-    	'''
-    	Add a case to the docket
+        '''
+        Add a case to the docket
 
-    	This command will add a case to the queue, for an upcoming 
-    	hearing. Currently the case chosen is random.
-    	'''
-        CaseQueue.choose()
-    	pass
+        This command will add a case to the queue, for an upcoming 
+        hearing. Currently the case chosen is random.
+        '''
+        if self.caseq.choose():
+            debug("successfully chose new case")
+        else:
+            debug("something went wrong")
+
+        pass
 
     @commands.has_role('Clerk')
     @commands.command(name='lineup')
