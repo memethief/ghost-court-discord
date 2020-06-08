9fd016866d39d51da2bba3212d9cf8562e8de3f4
Dave Leaman <github@memethief.com>
2020-06-08 01:57:22 -0400

    Implement nextcase command

--- clerk.py
+++ clerk.py
@@ -28,6 +28,44 @@ class ClerkCog(commands.Cog, name="Clerk commands"):
         pass
 
     @commands.has_role('Clerk')
+    @commands.command(name='nextcase')
+    async def next_case(self, ctx):
+        '''
+        Go to the next case
+
+        This command will end the current case (if any) and attempt
+        to start the next one. If any roles are vacant, the next 
+        case will fail to load.
+        '''
+        response = list()
+        lineup = self.roleq.lineup()
+        missing = list()
+        # Check to make sure all roles are assigned
+        for role, member in lineup.items():
+            if member is None:
+                missing.append(role)
+        if len(missing) == 0:
+            # Send back an error
+            response.append("Cannot start the next case. Must add "
+                "members to the following role(s):")
+            response.append(", ".join(missing))
+            await ctx.send("\n".join(status))
+            return
+
+        # if there is a current case, archive it
+        if not self.caseq.current is None:
+            self.caseq.history.append(self.caseq.current)
+            self.caseq.current = None
+
+        # if we have cases on the docket, make the next one current
+        if self.caseq.choose():
+            debug("successfully chose new case")
+        else:
+            debug("something went wrong")
+
+        pass
+
+    @commands.has_role('Clerk')
     @commands.command(name='lineup')
     async def rolequeue_lineup(self, ctx):
         '''
